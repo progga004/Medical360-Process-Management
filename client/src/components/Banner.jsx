ea1364cfd22e300e55c08643e6a5dd913db65719
jamie <jamie@barkume.com>
2024-04-19 16:50:54 -0400

    client/src/*

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = ({ goBackPath, showGoBackButton = true }) => { // Added showGoBac
 
   return (
     <div className="flex items-center justify-between bg-blue-500 py-5 px-8 text-white mb-4 mr-5 w-full">
-      <Link to="/apppage" className="text-2xl font-bold">Stony Brook Medical 360</Link>
+      <div className="text-2xl font-bold">Stony Brook Medical 360</div>
       <div className="flex items-center space-x-4">
         {showGoBackButton && ( // Use the new prop to conditionally render the Go Back button
           <button onClick={() => navigate(goBackPath)} className="text-sm font-medium bg-white text-blue-500 py-2 px-4 rounded hover:bg-blue-100">
