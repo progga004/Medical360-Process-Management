7a1499b546a54c157136fa627fd0137802c6c282
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-05-03 14:24:48 -0400

    work on all bugs and all feedback page and this for admin viewing

--- server/routes/bug-router.js
+++ server/routes/bug-router.js
@@ -5,10 +5,10 @@ const BugController = require('../controllers/bug-controller');
 // POST route to report a new bug
 router.post('/', BugController.createBug);
 
-// GET route to get all reported bugs
-router.get('/all', BugController.getAllBugs);
+// POST route to get all reported bugs
+router.post('/all', BugController.getAllBugs);
 
-// GET route to get a single bug report by id
-router.get('/:id', BugController.getBug);
+// POST route to get a single bug report by id
+router.post('/:id', BugController.getBug);
 
 module.exports = router;
