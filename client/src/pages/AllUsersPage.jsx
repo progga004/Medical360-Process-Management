91fc031536772a088f721989371846c5246d30d1
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-05-03 18:11:00 -0400

    fix search for users

--- client/src/pages/AllUsersPage.jsx
+++ client/src/pages/AllUsersPage.jsx
@@ -1,78 +1,67 @@
-import React, { useContext, useEffect, useState } from "react";
-import Banner from "../components/Banner";
-import Table from "../components/Table";
-import SearchBar from "../components/SearchBar";
-import { useAuthContext } from "../hooks/useAuthContext";
-import { useGlobalContext } from "../hooks/useGlobalContext";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import Banner from '../components/Banner';
+import Table from '../components/Table';
+import SearchBar from '../components/SearchBar';
+import { Link } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { useGlobalContext } from '../hooks/useGlobalContext';
 
 const AllUsersPage = () => {
-  const { user } = useAuthContext();
-  const { getAllDepartments, getAllUsers, users } = useGlobalContext();
-  const [filteredUsers, setFilteredUsers] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+    const { user } = useAuthContext();
+    const { getAllDepartments, getAllUsers, users } = useGlobalContext();
+    const [filteredUsers, setFilteredUsers] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect(() => {
-    localStorage.setItem("lastRoute", "/all-users");
-    console.log(users);
-    const fetchUsers = async () => {
-      if (user && !users) {
-        await getAllDepartments();
-        await getAllUsers();
-      }
-    };
-    fetchUsers();
+    useEffect(() => {
+        localStorage.setItem("lastRoute", "/all-users");
+        async function fetchUsers() {
+            if (user && !users) {
+                await getAllDepartments();
+                await getAllUsers();
+            }
+        }
+        fetchUsers();
 
-    return () => {
-      localStorage.removeItem("lastRoute");
-    };
-  }, [user, users]); // Re-fetch when auth.token changes
+        return () => {
+            localStorage.removeItem("lastRoute");
+        };
+    }, [user, users]);
 
-  useEffect(() => {
-    if (searchTerm) {
-      const lowercasedTerm = searchTerm.toLowerCase();
-      const filtered = users.filter(
-        (user) =>
-          user.name.toLowerCase().includes(lowercasedTerm) ||
-          user.email.toLowerCase().includes(lowercasedTerm) ||
-          (user.department &&
-            user.department.name.toLowerCase().includes(lowercasedTerm))
-      );
-      setFilteredUsers(filtered);
-    } else {
-      setFilteredUsers(users);
-    }
-  }, [searchTerm, users]);
+    useEffect(() => {
+        if (searchTerm) {
+            const lowercasedTerm = searchTerm.toLowerCase();
+            const filtered = users.filter(user =>
+                user.name.toLowerCase().includes(lowercasedTerm) ||
+                user.email.toLowerCase().includes(lowercasedTerm)
+            );
+            setFilteredUsers(filtered);
+        } else {
+            setFilteredUsers(users);
+        }
+    }, [searchTerm, users]);
 
-  return (
-    <>
-      <Banner goBackPath="/resource-management" />
-      <div className="flex justify-center my-4">
-        <h1 className="text-3xl font-bold text-blue-500">All Users</h1>
-      </div>
-      <div className="flex justify-between items-center mx-8 mb-4">
-        <SearchBar onSearch={setSearchTerm} />
-        {user && user.isAdmin && (
-          <Link
-            to={"/register"}
-            className="bg-[#2260FF] text-white px-2 py-1 rounded-md font-medium text-xl"
-          >
-            New User
-          </Link>
-        )}
-      </div>
-      <div className="p-8">
-        {users && (
-          <Table
-            cards={filteredUsers}
-            isAdmin={user && user.isAdmin}
-            context="user"
-          />
-        )}
-        {!users && <p>No user data available.</p>}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Banner goBackPath="/resource-management" />
+            <div className="flex justify-center my-4">
+                <div className="text-blue-500 p-4 rounded-lg text-3xl">
+                    All Users
+                </div>
+            </div>
+            <div className="flex justify-between items-center mx-8 mb-4">
+                <SearchBar onSearch={setSearchTerm} />
+                {user && user.isAdmin && (
+                    <Link to={"/register"} className="bg-[#2260FF] text-white px-2 py-1 rounded-md font-medium text-xl">
+                        New User
+                    </Link>
+                )}
+            </div>
+            <div className="p-8">
+                {filteredUsers && <Table cards={filteredUsers} isAdmin={user && user.isAdmin} context={"user"} />}
+                
+            </div>
+        </>
+    );
 };
 
 export default AllUsersPage;
