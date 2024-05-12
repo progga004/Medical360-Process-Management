3b3ee121bc18911fb6a36487ed1588d918fe471f
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-05-12 12:14:17 -0400

    restrict routes so it can only be accessed after logining and add validation for forms

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -37,13 +37,6 @@ const Sidebar = () => {
             </Link>
           </li>
         )}
-        {(user.name == "Admin") && (
-          <li>
-            <Link to="/departmentratio" className="hover:underline">
-              Department Ratio
-            </Link>
-          </li>
-        )}
         {(user.name == "Admin" ) && (
           <li>
             <Link to="/resource-management" className="hover:underline">
--- client/src/pages/AppPage.jsx
+++ client/src/pages/AppPage.jsx
@@ -45,12 +45,6 @@ const AppPage = () => {
             >
               View All Doctors
             </Link>
-            <Link
-              to="/unscheduled"
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-4"
-            >
-              View Available Doctors
-            </Link>
           </div>
         </div>
       </div>
