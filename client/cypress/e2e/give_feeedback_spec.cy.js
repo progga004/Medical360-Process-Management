3b1b44ce71329e558b8146dab14a27edae367388
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-05-14 14:31:15 -0400

    fix

--- client/cypress/e2e/give_feeedback_spec.cy.js
+++ client/cypress/e2e/give_feeedback_spec.cy.js
@@ -1,7 +1,7 @@
 describe("give feedback", () => {
     beforeEach(() => {
       cy.visit("/login");
-      cy.get("#Email").type("rovbiged@mimle.pw");
+      cy.get("#Email").type("mujmove@ufa.ws");
       cy.get("#Password").type("password@123");
       cy.get("button").contains("Login").click();
   
@@ -11,8 +11,8 @@ describe("give feedback", () => {
       
     });
     it("should allow filling the form and submitting it", () => {
-      cy.get("[name='name']").type("Gertrude Lawrence");
-      cy.get("[name='email']").type("rovbiged@mimle.pw");
+      cy.get("[name='name']").type("Peter Brooks");
+      cy.get("[name='email']").type("mujmove@ufa.ws");
         // Click on the third star for the rating
     cy.get(".text-gray-400").eq(4).click();
       cy.get("[name='comments']").type("This page is great");
