3d30b03235412bd504471fcc4e80ff9d3c4c271b
Progga <proggaparomita.dutta@stonybrook.edu>
2024-05-13 20:52:36 -0400

    User image

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import { AuthContextProvider } from './context/AuthContext.jsx'
 import { GlobalContextProvider } from './context/GlobalContext.jsx'
 
